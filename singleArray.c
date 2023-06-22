446c433575c21466f4cea4f20e006edb83c712a6
franklinMN <franklinmn1998@gmail.com>
2023-06-22 07:03:37 +0530

    changed

--- singleArray.c
+++ singleArray.c
@@ -6,6 +6,25 @@ int main() {
     int size = sizeof(array) / sizeof(array[0]);
 
     for ( int i=0 ; i<size ; i++ ) {
-        printf( "Value[%d] is %d", i , array[i] )
+        printf( "Value[%d] is %d\n", i , array[i] );
+    }
+
+    printf( "Sorting Process...\n" );
+
+    for( int i=0 ; i<size ; i++ ){
+        int minValue;
+        for( int j=i+1 ; j<size ; j++ ) {
+            if ( array[i]>array[j] ) {
+                minValue = array[j];
+                array[j] = array[i];
+                array[i] = minValue;
+            }
+        }
+    }
+
+    printf( "After Sorting Process...\n" );
+
+    for ( int i=0 ; i<size ; i++ ) {
+        printf( "Value[%d] is %d\n", i , array[i] );
     }
 }
